e85c493f10ca6ab039babbdd5709b8f9979b2b8b
darosss <darosss12@proton.me>
2025-07-18 00:00:03 +0200

    feat(settings): add ModifierType enum

---
+++ src/main/java/com/textbasedgame/settings/ModifierType.java
@@ -0,0 +1,14 @@
+package com.textbasedgame.settings;
+
+public enum ModifierType {
+    XP,
+    ITEMS_LOOT,
+    BETTER_RARITY,
+    BETTER_RARITY_UNCOMMON,
+    BETTER_RARITY_EPIC,
+    BETTER_RARITY_LEGENDARY,
+    BETTER_RARITY_MYTHIC,
+    SPEED,
+    ;
+
+}
