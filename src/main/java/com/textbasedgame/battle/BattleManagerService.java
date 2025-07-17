b41d0471d4e8fafdb0bb79de0f7f02027c337e6a
darosss <darosss12@proton.me>
2025-07-18 01:18:11 +0200

    feat(BattleManagerService): inject GoldService and use it in new instances of Fights

--- src/main/java/com/textbasedgame/battle/BattleManagerService.java
+++ src/main/java/com/textbasedgame/battle/BattleManagerService.java
@@ -6,6 +6,7 @@ import com.textbasedgame.enemies.Enemy;
 import com.textbasedgame.settings.LootService;
 import com.textbasedgame.settings.XpService;
 import com.textbasedgame.users.User;
+import com.textbasedgame.utils.GoldService;
 import org.springframework.stereotype.Service;
 import java.util.*;
 
@@ -13,15 +14,17 @@ import java.util.*;
 public class BattleManagerService {
     //TODO: add reports into database
     private final XpService xpService;
-    private final LootService lootService   ;
+    private final LootService lootService;
+    private final GoldService goldService;
 
-    public BattleManagerService(XpService xpService, LootService lootService) {
+    public BattleManagerService(XpService xpService, LootService lootService, GoldService goldService) {
         this.xpService = xpService;
         this.lootService = lootService;
+        this.goldService = goldService;
     }
 
     public FightReport performNormalFight(User user, List<Character> characters, List<Enemy> enemies, int mainHeroLevel) {
-        Fight fightInstance = new Fight(this.xpService, this.lootService, user, characters, enemies, mainHeroLevel, false);
+        Fight fightInstance = new Fight(this.xpService, this.lootService, this.goldService, user, characters, enemies, mainHeroLevel, false);
 
 
         //TODO: here we will need to checks whether fight is win - give xp, gold etc
@@ -34,12 +37,12 @@ public class BattleManagerService {
 
 
     public FightReport performFight(User user, Character character, Enemy enemy, int mainHeroLevel){
-        Fight fightInstance = new Fight(this.xpService, this.lootService, user, List.of(character), List.of(enemy), 50, mainHeroLevel, false);
+        Fight fightInstance = new Fight(this.xpService, this.lootService, this.goldService, user, List.of(character), List.of(enemy), 50, mainHeroLevel, false);
         fightInstance.startFight();
         return fightInstance.getFightReport();
     }
     public FightReport performTeamFight(User user, List<Character> characters, List<Enemy> enemies, int mainHeroLevel){
-        Fight fightInstance = new Fight(this.xpService, this.lootService, user, characters, enemies, 300, mainHeroLevel, true);
+        Fight fightInstance = new Fight(this.xpService, this.lootService, this.goldService, user, characters, enemies, 300, mainHeroLevel, true);
         fightInstance.startFight();
         return fightInstance.getFightReport();
     }
