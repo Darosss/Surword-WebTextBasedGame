174bc4ffa589061a5113ab8eb3ce79f6f6502d29
darosss <darosss12@proton.me>
2025-07-18 00:02:01 +0200

    refactor(auth): rename LoggedUserUtils into ...Service + add usage of xpService

--- src/main/java/com/textbasedgame/auth/LoggedUserService.java
+++ src/main/java/com/textbasedgame/auth/LoggedUserService.java
@@ -2,33 +2,38 @@ package com.textbasedgame.auth;
 
 import com.textbasedgame.characters.MainCharacter;
 import com.textbasedgame.common.ResourceNotFoundException;
+import com.textbasedgame.settings.XpService;
 import com.textbasedgame.users.User;
 import com.textbasedgame.users.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-public class LoggedUserUtils {
-
-    private LoggedUserUtils() {
+@Service
+public class LoggedUserService {
+    private final XpService xpService;
+    private LoggedUserService(XpService xpService) {
+        this.xpService = xpService;
     }
 
 
     public record MainCharacterDetails (int health, int maxHealth, int level, long experience, long neededExp){}
     public record ProfileUserDetails(User user, Optional<MainCharacterDetails> heroDetails){}
-    public static User getLoggedUserDetails(AuthenticationFacade authenticationFacade, UserService userService) throws Exception {
+    public User getLoggedUserDetails(AuthenticationFacade authenticationFacade, UserService userService) throws Exception {
         String userId = authenticationFacade.getJwtTokenPayload().id();
         Optional<User> foundUser = userService.findOneById(userId);
         return foundUser.orElseThrow(() -> new ResourceNotFoundException("Not found user data. Please try again later"));
     }
-    public static ProfileUserDetails getProfileUserDetails(AuthenticationFacade authenticationFacade, UserService userService) throws Exception {
+    public ProfileUserDetails getProfileUserDetails(AuthenticationFacade authenticationFacade, UserService userService) throws Exception {
         User user = getLoggedUserDetails(authenticationFacade, userService);
         return new ProfileUserDetails(user, getMainCharacterDetails(user));
     }
 
-    public static Optional<MainCharacterDetails> getMainCharacterDetails(User user) {
+    public Optional<MainCharacterDetails> getMainCharacterDetails(User user) {
         Optional<MainCharacter>  mainCharacter = user.getMainCharacter();
         return mainCharacter.map((character)->new MainCharacterDetails(
-                character.getHealth(), character.getStats().getMaxHealthEffValue(), character.getLevel(), character.getExperience(), character.getExpToLevelUp()
+                character.getHealth(), character.getStats().getMaxHealthEffValue(), character.getLevel(), character.getExperience(),
+                this.xpService.getNeededXpForNextLevel(character.getLevel())
         ));
     }
 }
