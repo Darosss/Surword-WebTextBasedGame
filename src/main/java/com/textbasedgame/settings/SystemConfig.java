e3fa25c04abcd4da94a5785c6c2bab6f61ea1f84
darosss <darosss12@proton.me>
2025-07-17 23:54:43 +0200

    feat(SystemConfig): add initial SystemConfig entity

---
+++ src/main/java/com/textbasedgame/settings/SystemConfig.java
@@ -0,0 +1,38 @@
+package com.textbasedgame.settings;
+
+import dev.morphia.annotations.ExternalEntity;
+
+import java.util.List;
+
+@ExternalEntity(target = SystemConfig.class)
+public class SystemConfig{
+        private final long leaderboardRefreshCooldownMs;
+        private final List<MercenaryLimit> mercenaryCharacterLimits;
+
+    public SystemConfig(long leaderboardRefreshCooldownMs, List<MercenaryLimit> mercenaryCharacterLimits) {
+        this.leaderboardRefreshCooldownMs = leaderboardRefreshCooldownMs;
+        this.mercenaryCharacterLimits = mercenaryCharacterLimits;
+    }
+
+    public record MercenaryLimit(int requiredLevel, int charactersLimit) { }
+
+    public long getLeaderboardRefreshCooldownMs() {
+        return leaderboardRefreshCooldownMs;
+    }
+
+    public List<MercenaryLimit> getMercenaryCharacterLimits() {
+        return mercenaryCharacterLimits;
+    }
+
+    public static SystemConfig defaults() {
+        return new SystemConfig(
+                1_000 * 60 * 60 * 24,
+                List.of(
+                        new MercenaryLimit(10, 2),
+                        new MercenaryLimit(30, 3),
+                        new MercenaryLimit(50, 4)
+                )
+        );
+    }
+}
+
